621efee477a5fee4a1f21f04fdccb01dca72a85d
Hanna <hreed3@knights.ucf.edu>
2021-03-29 14:36:15 -0400

    Fixed comments

--- Data_Fitting_LS.py
+++ Data_Fitting_LS.py
@@ -9,13 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate, optimize
 
-#Here is the data we are going to fit: 
-#infectives I at time t
+# Here is the data we are going to fit:
+# infectives I at time t
 time = np.linspace(0,10,10)
 infectives = np.array([1. , 8.6556056 , 9.37871953 , 8.44328311 ,
                        7.55728868 , 6.76265894 , 6.05150019 , 
                        5.41512095 , 4.84566321 , 4.33608991])
-#Known inital conditions:
+# Known inital conditions:
 y0 = np.array([10, 1, 0])
 
 # the true best parameters are beta = 0.33 and gamma = 0.1
@@ -46,7 +46,7 @@ def LS(x0, t, y0, data):
     return m
 
 
-#initial guess
+# initial guess
 x0 = np.array([0,0])
 t = np.linspace(0,10,10)
 m = LS(x0, t, y0, infectives)
@@ -58,10 +58,10 @@ print('these are the best fit parameters [beta, gamma]: ',m.x)
 t = np.linspace(0, 10, 100)
 y = integrate.odeint(model, y0, t, args = (m.x[0], m.x[1]))
 
-#plot the data
+# plot the data
 plt.plot(np.linspace(0,len(infectives), len(infectives)), infectives, 'ro', label = 'Data')
 
-#plot the solutions
+# plot the solutions
 plt.plot(t, y[:,0], 'b', label = 'Susceptibles')
 plt.plot(t, y[:,1], 'r', label = 'Infectives')
 plt.plot(t, y[:,2], 'g', label = 'Recovered')
